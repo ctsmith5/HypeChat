6e3b21169e44c9ec65551a1c8205e806e24cbab0
Colin Smith <devcolin@icloud.com>
2019-07-09 10:11:22 -0600

    First commit

---
+++ HYPEchat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HYPEchat
+//
+//  Created by Colin Smith on 7/9/19.
+//  Copyright © 2019 Colin Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
